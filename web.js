3578b69341f0f23f70c7714901b5f86e65d289cb
Ubuntu <ubuntu@ip-172-31-23-207.us-west-2.compute.internal>
2013-07-10 02:02:57 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 var file = fopen(getScriptPath("index.html"),o));
 app.get('/',function(request,response){
-   response.send(file);
+   response.send("KK");
 });
 
 var port = process.env.PORT || 5000;
