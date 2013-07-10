43d3748d89c1c1d7b278bdd88a6b04461de5daa1
Ubuntu <ubuntu@ip-172-31-23-207.us-west-2.compute.internal>
2013-07-10 02:35:53 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -6,8 +6,6 @@ var fs = require('fs');
 
 fs.readFileSync('index.html', function (err, data) {
   if (err) throw err;
-  console.log(data);
-
 
 app.get('/',function(request,response){
    response.send(data);
