c7d6cc8cb48a39dbac67e7fe1d2324fa1d527979
Ubuntu <ubuntu@ip-172-31-23-207.us-west-2.compute.internal>
2013-07-10 02:43:06 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -4,13 +4,12 @@ var app = express.createServer(express.logger());
 
 var fs = require('fs');
 
-fs.readFileSync('index.html', function (err, data) {
+fs.readFile('/etc/passwd', function (err, data) {
   if (err) throw err;
+  app.get('/',function(request,response)[response.send(data);
+});
 
-app.get('/',function(request,response){
-   response.send(data);
-});
-});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function(){
 console.log("Listening on " + port);
